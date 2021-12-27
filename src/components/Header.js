d0dd9328abfd836e494e225528702225fa2d4432
Krishnamoorti Bhat <69892786+KMmoodgar@users.noreply.github.com>
2021-12-27 13:19:11 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import './Header.css'
+
+function Header() {
+
+
+  return (
+    <div className="header">
+      <h1>
+        Emoji Search
+      </h1>
+      <p>
+        A simple emoji search made with ReactJS.
+      </p>
+    </div>
+  )
+}
+
+export default Header
